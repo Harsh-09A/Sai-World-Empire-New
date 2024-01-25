94396d5e5adb3d9b783e6325d4fe6e996cde7ab5
Harsh Athavale <harsh.autowebbed@gmail.com>
2024-01-25 17:46:42 +0530

    Completed

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(window).ready(function () {
+  setTimeout(function () {
+    $("#exampleModal").modal("show");
+  }, 3000);
+});
+
+// Toggle Content Button
+function toggleContent(dotsId, moreTextId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreTextId);
+  var btnText = document.getElementById(btnId);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More ...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
